c7a744738370d461ce53fda5baa0f201ed35ed0b
Jeremiah Almazar <jemalmazar@gmail.com>
2016-02-05 18:33:22 +0000

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(function() {
+
+  // bxSlider
+  $('.bxslider').bxSlider({
+    controls: false,
+    auto: true,
+    infiniteLoop: true,
+    maxSlides: 4,
+    slideMargin: 0,
+    slideWidth: 0
+
+  });
+
+  // Smooth Scroll
+
+  $('a').click(function(){
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+  });
+
+  // Subscription Alert
+
+  $('form').on('submit', function(event) {
+
+     event.preventDefault();
+     if ($('input[type="email"]').val().length !== 0 ) {
+        alert('Thank you for subscribing!');
+     }
+     else
+     {
+        alert('Please enter a valid email address.');
+     }
+  });
+
+});
